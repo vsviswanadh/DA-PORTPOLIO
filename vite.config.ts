4f2f80f7c921e45242214c6fd77db6bfdfb915a6
vsviswanadh <168512601+vsviswanadh@users.noreply.github.com>
2025-05-30 16:44:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/DA-PORTFOLIO/', // 👈 set this to your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
